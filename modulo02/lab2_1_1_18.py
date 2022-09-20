05415f329dd53f5406f22ba9d82e75d4b10618d8
gvenado28 <jaimevenado.1216@gmail.com>
2022-09-20 09:53:48 -0500

    laboratorio 2 1 1 18

---
+++ modulo02/lab2_1_1_18.py
@@ -0,0 +1,8 @@
+''''
+Descripcion: Fundamentos de programación en Python
+Autor: German Venado
+Fecha: 20 sep 2022
+'''
+
+print("Fundamentos","Programación","en", sep="***", end="..")
+print("Python")
---
+++ modulo02/lab2_1_1_19.py
@@ -0,0 +1,33 @@
+''''
+Descripcion: Hacer la flecha al doble y duplicarla de lado a lado
+Autor: German Venado
+Fecha: 20 sep 2022
+'''
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+print("Flecha al doble")
+print("         *")
+print("        * *")
+print("       *   *")
+print("      *     *")
+print("     *       *")
+print("    *         *")
+print("   *           *")
+print("  *             *")
+print(" *               *")
+print("* * * *     * * * *")
+print("      *     *")
+print("      *     *")
+print("      *     *")
+print("      *     *")
+print("      *     *")
+print("      *     *")
+print("      * * * *")
