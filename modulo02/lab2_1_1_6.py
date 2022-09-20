6c49f73760abe6072b99c1195472ce2769a5d1fc
gvenado28 <jaimevenado.1216@gmail.com>
2022-09-20 08:58:56 -0500

    laboratorio 2 1 1 6

---
+++ modulo02/lab2_1_1_6.py
@@ -0,0 +1,9 @@
+''''
+Descripcion: Programa muy fácil de la función Print
+Autor: German Venado
+Fecha: 20 sep 2022
+'''
+
+print("Hola Mundo!!!")
+print("Soy German ")
+print("Bienvenido a Python")
