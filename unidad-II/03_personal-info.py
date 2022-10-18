263a900da3c7df1d574dfc3c1d35c7a8adc0ba48
gvenado28 <106276803+gvenado28@users.noreply.github.com>
2022-10-18 17:10:42 -0500

    Add files via upload

---
+++ unidad-II/03_personal-info.py
@@ -0,0 +1,7 @@
+name = input("¿Cuál es tu nombre?")
+last_name = input("¿Cuál es tu apellido?")
+ubi = input("¿Dónde vives?")
+age = input("¿Qué edad tienes?")
+space = " "
+
+print("Nombre:",name,space,"Apellido:",last_name,space,"Localidad:",ubi,space,"Edad:",age)
