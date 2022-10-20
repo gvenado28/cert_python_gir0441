9418f18afbab5b9038a84d04998af0f49f54356d
gvenado28 <106276803+gvenado28@users.noreply.github.com>
2022-10-20 10:07:34 -0500

    Mi-Solución-y-Corección

--- unidad-II/07_file-access_activity.py
+++ unidad-II/07_file-access_activity.py
@@ -3,9 +3,24 @@ opc = 'exit'
 
 while True:
     newItem = input('¿Qué dispositivo ',+
-    'desea agregar?')
-    a.write(newItem + “\n”)
+    'desea agregar? ')
+    a.write(newItem + '\n')
     if opc == 'exit':
         break
     print('¡Todo listo!')
 
+'''
+CORECCIÓN
+
+file = open("devices.txt", "a")
+newItem = ''
+
+while True:
+    newItem = input('¿Qué dispositivo desea agregar? ')
+
+    if newItem == 'exit':
+        print('¡Todo listo!')
+        break
+    
+file.write(newItem + '\n')
+file.close()'''
